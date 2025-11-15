1595e74b1bc9ec7316f7e40a0b7810a81f75da4f
Santiago Rodriguez <santiago.rodriguez9@utp.edu.co>
2025-11-14 21:20:22 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="h-screen bg-[#222222]">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="h-screen text-9xl text-white flex items-center justify-center">
+    This is the principal home
+  </main>
+</template>
